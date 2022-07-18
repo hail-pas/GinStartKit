16576883c6b0a0f495545ff66b0137ef378227e9
panhq <hypofiasco@gmail.com>
2022-07-18 17:52:33 +0800

    rename sql function for set timestamp

--- storage/relational/migrations/000001_initial.up.sql
+++ storage/relational/migrations/000001_initial.up.sql
@@ -18,7 +18,10 @@ comment on column "user".deleted_at is '删除时间戳';
 drop table if exists "operation_record";
 create table if not exists "operation_record"
 (
-
+    id         bigint primary key,
+    created_at timestamptz not null,
+    updated_at timestamptz not null,
+    deleted_at timestamptz null
 
 );
 comment on table "operation_record" is '操作记录表';
@@ -29,7 +32,7 @@ comment on column "operation_record".deleted_at is '删除时间戳';
 COMMIT;
 
 --创建更新时间戳trigger
-CREATE OR REPLACE FUNCTION func_set_create_timestamp()
+CREATE OR REPLACE FUNCTION func_create_set_timestamp()
     RETURNS TRIGGER AS
 $$
 BEGIN
@@ -39,7 +42,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION func_set_update_timestamp()
+CREATE OR REPLACE FUNCTION func_update_set_timestamp()
     RETURNS TRIGGER AS
 $$
 BEGIN
@@ -53,22 +56,22 @@ CREATE TRIGGER trigger_set_update_timestamp
     BEFORE INSERT
     ON "user"
     FOR EACH ROW
-EXECUTE PROCEDURE func_set_create_timestamp();
+EXECUTE PROCEDURE func_create_set_timestamp();
 
 CREATE TRIGGER trigger_set_update_timestamp
     BEFORE UPDATE
     ON "user"
     FOR EACH ROW
-EXECUTE PROCEDURE func_set_update_timestamp();
+EXECUTE PROCEDURE func_update_set_timestamp();
 
 CREATE TRIGGER trigger_set_update_timestamp
     BEFORE INSERT
     ON "operation_record"
     FOR EACH ROW
-EXECUTE PROCEDURE func_set_create_timestamp();
+EXECUTE PROCEDURE func_create_set_timestamp();
 
 CREATE TRIGGER trigger_set_update_timestamp
     BEFORE UPDATE
     ON "operation_record"
     FOR EACH ROW
-EXECUTE PROCEDURE func_set_update_timestamp();
+EXECUTE PROCEDURE func_update_set_timestamp();
